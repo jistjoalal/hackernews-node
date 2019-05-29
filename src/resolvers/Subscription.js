f156523f8e4f775ce6d720116914c47545586722
jistjoalal <jistjoalal@gmail.com>
2019-05-29 16:27:42 -0700

    add newVote subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,4 +1,4 @@
-function newLinkSubscribe(parents, args, ctx, info) {
+function newLinkSubscribe(parent, args, ctx, info) {
   return ctx.prisma.$subscribe.link({ mutation_in: ["CREATED"] }).node();
 }
 
@@ -7,6 +7,16 @@ const newLink = {
   resolve: payload => payload
 };
 
+function newVoteSubscribe(parent, args, ctx, info) {
+  return ctx.prisma.$subscribe.vote({ mutation_in: ["CREATED"] }).node();
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => payload
+};
+
 module.exports = {
-  newLink
+  newLink,
+  newVote
 };
