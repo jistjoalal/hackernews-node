fa7579313d174b60520685307eafc75d2d11cf39
jistjoalal <jistjoalal@gmail.com>
2019-05-29 16:12:07 -0700

    functioning JWT signup/login

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 // resolvers define schema implementation
 const resolvers = {
-  Query: {
-    info: () => "This is the API of a hackernews clone",
-    feed: (root, args, ctx, info) => {
-      return ctx.prisma.links();
-    }
-  },
-  Mutation: {
-    post: (root, args, ctx) => {
-      return ctx.prisma.createLink({
-        url: args.url,
-        description: args.description
-      });
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 // pass both to server
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma }
+  context: req => {
+    return {
+      ...req,
+      prisma
+    };
+  }
 });
 server.start(() => console.log(`server online @ http://localhost:4000`));
