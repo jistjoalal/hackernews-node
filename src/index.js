74cc6cae47183840eac2279d6784c0a2b42ad307
jistjoalal <jistjoalal@gmail.com>
2019-05-29 14:28:36 -0700

    links query

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+// dummy data
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "fullstack tutorial"
+  }
+];
+
 // typeDefs define GraphQL schema
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 // resolvers define schema implementation
 const resolvers = {
   Query: {
-    info: () => `This is the API of a hackernews clone`
+    info: () => "This is the API of a hackernews clone",
+    feed: () => links
   }
 };
 
