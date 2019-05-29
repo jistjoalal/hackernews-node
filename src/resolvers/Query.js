52958571b664f5d628d5139a3ab5185ef776511f
jistjoalal <jistjoalal@gmail.com>
2019-05-29 16:32:32 -0700

    link filtering/search feature

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,14 @@
-function feed(parent, args, ctx, info) {
-  return ctx.prisma.links();
+async function feed(parent, args, ctx, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {};
+  const links = await ctx.prisma.links({ where });
+  return links;
 }
 
 module.exports = {
