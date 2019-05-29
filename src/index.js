8f8540422f8572c07a5fc851a1ddf66aba81297c
jistjoalal <jistjoalal@gmail.com>
2019-05-29 14:43:35 -0700

    more simple queries/mutations

--- src/index.js
+++ src/index.js
@@ -8,12 +8,38 @@ let links = [
     description: "fullstack tutorial"
   }
 ];
+let idCount = links.length;
 
 // resolvers define schema implementation
 const resolvers = {
   Query: {
     info: () => "This is the API of a hackernews clone",
-    feed: () => links
+    feed: () => links,
+    link: (parent, args) => links.filter(({ id }) => id == args.id)[0]
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = links.filter(({ id }) => id == args.id)[0];
+      if (args.url) link.url = args.url;
+      if (args.description) link.description = args.description;
+      links = links.filter(({ id }) => id != args.id);
+      links.push(link);
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      const link = links.filter(({ id }) => id == args.id)[0];
+      links = links.filter(({ id }) => id != args.id);
+      return link;
+    }
   }
 };
 
